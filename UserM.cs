4367150201978eb72171a84687ce9344f984b92b
tama231 <150502624+tama231@users.noreply.github.com>
2026-08-26 15:23:14 +0700

    Add files via upload

--- UserM.cs
+++ UserM.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Panel Referensi")]
+    public GameObject mainPanel;
+    public GameObject optionsPanel;
+
+    [Header("Audio")]
+    public AudioSource bgMusic;
+    public Slider volumeSlider;
+
     void Start()
     {
-        
+        // Pastikan panel awal yang aktif hanya mainPanel
+        mainPanel.SetActive(true);
+        optionsPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Dipanggil dari tombol "Play"
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene("GameScene"); // ganti sesuai nama scene
+    }
+
+    // Dipanggil dari tombol "Options"
+    public void OpenOptions()
+    {
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    // Dipanggil dari tombol "Back" di panel options
+    public void BackToMain()
+    {
+        optionsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    // Dipanggil dari slider volume
+    public void SetVolume(float value)
+    {
+        bgMusic.volume = value;
+    }
+
+    // Dipanggil dari tombol "Quit"
+    public void QuitGame()
+    {
+        Debug.Log("Keluar game...");
+        Application.Quit();
     }
 }
